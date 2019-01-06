735a542989f8a393266481d3d1b51c0635db9ca7
Igor Ramos <igor.ramos@live.com>
2019-01-06 18:33:27 -0200

    até a aula 15

--- wp-content/plugins/snappy-list-builder-2/snappy-list-builder-2.php
+++ wp-content/plugins/snappy-list-builder-2/snappy-list-builder-2.php
@@ -17,12 +17,20 @@ Text Domain: snappy-list-builder-2
 
     1. HOOKS
         1.1 - registers all our custom shortcodes
+        1.2 - register custom admin column headers
+        1.3 - register column admin column data
 
     2. SHORTCODES
         2.1 - slb_register_shortcodes()
         2.2 - slb_form_shortcode()
 
     3. FILTERS
+        3.1 - slb_subscriber_column_headers()
+        3.2 - slb_subscriber_column_data()
+        3.2.2 - slb_register_custom_admin_titles()
+        3.2.3 - slb_custom_admin_titles()
+        3.3 - slb_list_column_headers()
+        3.4 - slb_list_column_data()
 
     4. EXTERNAL SCRIPTS
 
@@ -41,9 +49,21 @@ Text Domain: snappy-list-builder-2
 /* 1. HOOKS */
 
 //1.1
-
 //Hint: registers all our custom shortcodes on init
 add_action('init', 'slb_register_shortcodes');
+
+//1.2
+//Hint: registers all our custom shortcodes on init
+add_filter('manage_edit-slb_subscriber_columns', 'slb_subscriber_column_headers');
+add_filter('manage_edit-slb_list_columns', 'slb_list_column_headers');
+
+//1.3
+//Hint: Register custom admin column data
+add_filter('manage_slb_subscriber_posts_custom_column', 'slb_subscriber_column_data', 1, 2);
+add_action(
+    'admin_head-edit.php',
+    'slb_register_custom_admin_titles'
+);
 
 
 /* 2. SHORTCODES */
@@ -93,6 +113,108 @@ function slb_form_shortcode($args, $content = "") {
 
 
 /* 3. FILTERS */
+
+//3.1
+function slb_subscriber_column_headers($columns) {
+
+    //Creating custom column header data
+    $columns = array(
+        'cb' => '<input type="checkbox">',
+        'title' => __('Subscriber Name'),
+        'email' => __('Email Address')
+    );
+
+    // Return new columns
+    return $columns;
+}
+
+// 3.2
+function slb_subscriber_column_data($column, $post_id) {
+
+    // Setup our return text
+    $output = '';
+
+    switch($column) {
+        case 'title':
+            // Get the custom name data
+            $fname = get_field('slb_fname', $post_id);
+            $lname = get_field('slb_lname', $post_id);
+            $output .= $fname . ' ' . $lname;
+            break;
+        case 'email':
+            // Get the custom email data
+            $email = get_field('slb_email', $post_id);
+            $output .= $email;
+            break;
+    }
+
+    // Echo the output
+    echo $output;
+}
+
+// 3.2.2
+// Hint: registers special custom admin title columns
+function slb_register_custom_admin_titles() {
+    add_filter(
+        'the_title',
+        'slb_custom_admin_titles',
+        99,
+        2
+    );
+}
+
+// 3.2.3
+// Hint: handles custom admin title "title" column data for post types without titles
+function slb_custom_admin_titles($title, $post_id) {
+
+    global $post;
+    $output = $title;
+
+    if( isset($post->post_type) ):
+        switch( $post->post_type ) {
+            case 'slb_subscriber':
+                $fname = get_field('slb_fname', $post_id);
+                $lname = get_field('slb_lname', $post_id);
+                $output = $fname . ' ' . $lname;
+                break;
+        }
+    endif;
+
+    return $output;
+}
+
+//3.3
+function slb_list_column_headers($columns) {
+
+    //Creating custom column header data
+    $columns = array(
+        'cb' => '<input type="checkbox">',
+        'title' => __('List Name'),
+    );
+
+    // Return new columns
+    return $columns;
+}
+
+// 3.4
+function slb_list_column_data($column, $post_id) {
+
+    // Setup our return text
+    $output = '';
+
+    switch($column) {
+        case 'example':
+            // // Get the custom name data
+            // $fname = get_field('slb_fname', $post_id);
+            // $lname = get_field('slb_lname', $post_id);
+            // $output .= $fname . ' ' . $lname;
+            break;
+    }
+
+    // Echo the output
+    echo $output;
+}
+
 
 
 /* 4. EXTERNAL SCRIPTS */
